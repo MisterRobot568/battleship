a5cd77679afd781bbb11f3ab3badc26ebfba40b9
Austin <christopheraustinwallace@gmail.com>
2024-08-29 13:39:55 -0400

    add gameBoard class and associated testing

--- src/gameBoard.js
+++ src/gameBoard.js
@@ -3,7 +3,7 @@ class GameBoard {
     constructor() {
         this.board = [
             // 0 = water
-            // 1 = ship
+            // shipName = ship
             // 2 = hit
             // 3 = miss
             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -18,7 +18,8 @@ class GameBoard {
             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
         ];
         // use an object to store the ships?
-        this.shipsObj = {};
+        // this.shipsObj = {};
+        this.shipsMap = new Map();
     }
     // should be able to place ships at specific coords by calling the ship class
     // need to make sure ship is placed on valid coords, not outside the board
@@ -26,7 +27,7 @@ class GameBoard {
 
     // is placeship given one set of coordinates? or is given a Z number of coords
     // based on Z length of a ship?
-    placeShip(xCoords, yCoords) {
+    placeShip(shipName, x, y, length, horizontal) {
         //assume we're given arrays of coordinates.
         // 1) length of each array must match the ship length
         // 2) length of each array must match each other
@@ -39,7 +40,7 @@ class GameBoard {
         // inside this method check:
         // 1) pieces are not being placed on top of other pieces
         // 2)
-        let ship = new Ship(xCoords.length);
+        // let ship = new Ship(xCoords.length);
         // if (xCoords.length !== yCoords.length) {
         //     throw new Error('Placeship coordinate length mismatch');
         // }
@@ -64,18 +65,89 @@ class GameBoard {
         //         }
         //     }
         // }
-        this.shipsObj.ship = new Ship(xCoords.length);
-        this.shipsObj.xCoordinates = xCoords;
-        this.shipsObj.yCoordinates = yCoords;
+        // this.shipsObj.ship = new Ship(xCoords.length);
+        // this.shipsObj.xCoordinates = xCoords;
+        // this.shipsObj.yCoordinates = yCoords;
 
-        // place the ship on the board
-        for (let i = 0; i < xCoords.length; i++) {
-            if (this.board[yCoords[i]][xCoords[i]] === 1) {
-                throw new Error('Overlapping ships');
+        // // place the ship on the board
+        // for (let i = 0; i < xCoords.length; i++) {
+        //     if (this.board[yCoords[i]][xCoords[i]] === 1) {
+        //         throw new Error('Overlapping ships');
+        //     } else {
+        //         this.board[yCoords[i]][xCoords[i]] = 1;
+        // }
+        // let ship = new Ship(shipName, xCoords.length);
+        // for (let i = 0; i < xCoords.length; i++) {
+        //     this.board[yCoords[i]][xCoords[i]] = ship;
+        // }
+        ////////////////////////////////////////
+        // places pieces on board based on original coords, and
+        // horizontal/vertical
+        // we need to:
+        // 1) make sure the piece will not flow off of the board
+        // 2) make sure the piece isn't being placed on top of another piece
+        //////////////////////////////////////
+        let ship = new Ship(shipName, length);
+        for (let i = 0; i < length; i++) {
+            const posX = horizontal ? x + i : x;
+            const posY = horizontal ? y : y + i;
+            if (posX < 0 || posY < 0 || posX > 9 || posY > 9) {
+                throw new Error('Ship flowing off the board error');
+            }
+            // if no piece is already in that space
+            if (this.board[posY][posX] === 0) {
+                this.board[posY][posX] = shipName;
+
+                // if ship is not already in the map datastructure, add it
+                if (!this.shipsMap.has(shipName)) {
+                    this.shipsMap.set(shipName, ship);
+                }
+                // add associated coordinates to the ship's map data structure
+                // this.shipsMap.get(ship).push({ x: posX, y: posY });
             } else {
-                this.board[yCoords[i]][xCoords[i]] = 1;
+                throw new Error('Overlapping ships error');
             }
         }
     }
+    // takes a pair of coordinates, determines if the attack hit a ship
+    // sends hit function to the correct ship, or records coords of missed shot
+
+    // how should we record coords of missed shot? just on the map board itself or
+    // in a datastructure?
+
+    // don't allow attacks if it has already been attacked
+    receiveAttack(x, y) {
+        let target = this.board[y][x];
+
+        if (typeof target === 'string') {
+            // add a hit to the boat and change map
+            let currentShip = this.shipsMap.get(target);
+            currentShip.hit();
+            currentShip.isSunk();
+            this.board[y][x] = 2;
+        }
+        if (target === 0) {
+            this.board[y][x] = 3;
+            // maybe add this coord to a datastructure later?
+        }
+        if (target === 2) {
+            throw new Error('ship has already been hit here');
+        }
+        if (target === 3) {
+            throw new Error('target has already been missed here');
+        }
+    }
+
+    shipsSunk() {
+        for (const [shipName, shipDetails] of this.shipsMap) {
+            // if any ship is not sunk, return false
+            if (!shipDetails.sunk) {
+                return false;
+            }
+        }
+        // if all ships sunk, return true
+        return true;
+    }
 }
+
 module.exports = { GameBoard };
--- src/units.test.js
+++ src/units.test.js
@@ -8,20 +8,20 @@ const { Ship } = require('./ship');
 const { GameBoard } = require('./gameBoard');
 
 test('Ship constructor initializes with correct values', () => {
-    const ship = new Ship(3, 0);
+    const ship = new Ship(test, 3);
     expect(ship.length).toBe(3);
     expect(ship.timesHit).toBe(0);
     expect(ship.sunk).toBe(false);
 });
 
 test('Ship hit method properly increments the timesHit', () => {
-    const ship = new Ship(3, 0);
+    const ship = new Ship(test, 3);
     ship.hit();
     expect(ship.timesHit).toBe(1);
 });
 
 test('Ship isSunk method properly sinks ships', () => {
-    const ship = new Ship(3, 0);
+    const ship = new Ship(test, 3);
     ship.hit();
     ship.hit();
     ship.hit();
@@ -43,27 +43,123 @@ test('GameBoard initializes with correct values', () => {
         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
     ]);
-    expect(gameBoard.shipsObj).toEqual({});
+    expect(gameBoard.shipsMap.size).toEqual(0);
 });
 
-test('GameBoard placeShip() properly places a ship', () => {
+test('GameBoard placeShip() properly places a ship if there are not yet ships', () => {
     const gameBoard = new GameBoard();
-    gameBoard.placeShip([2, 2, 2], [2, 3, 4]);
+    gameBoard.placeShip('test', 2, 2, 3, true);
     expect(gameBoard.board).toEqual([
         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 1, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 1, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 1, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 'test', 'test', 'test', 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
     ]);
-    expect(gameBoard.shipsObj).toEqual({
-        ship: { length: 3, timesHit: 0, sunk: false },
-        xCoordinates: [2, 2, 2],
-        yCoordinates: [2, 3, 4],
+    // expect(Array.from(gameBoard.shipsMap)).toEqual([
+    //     [
+    //         'test',
+    //         [
+    //             { x: 2, y: 2 },
+    //             { x: 3, y: 2 },
+    //             { x: 4, y: 2 },
+    //         ],
+    //     ],
+    // ]);
+    let testShip = new Ship('test', 3);
+    expect(gameBoard.shipsMap.get('test')).toEqual(testShip);
+});
+
+// gameboard error handling
+describe('GameBoard placeShip() error handling', () => {
+    let gameBoard;
+
+    //creates a new independent gameboard before each test
+    beforeEach(() => {
+        gameBoard = new GameBoard();
+    });
+
+    test('GameBoard throws error if ship flows off board horizontally', () => {
+        expect(() => {
+            gameBoard.placeShip('test', 7, 0, 5, true);
+        }).toThrow('Ship flowing off the board error');
+    });
+
+    test('GameBoard throws an error if ship flows off the board vertically', () => {
+        expect(() => {
+            gameBoard.placeShip('test', 9, 9, 5, false);
+        }).toThrow('Ship flowing off the board error');
+    });
+
+    test('GameBoard throws an error is there is an overlapping ship', () => {
+        gameBoard.placeShip('test', 2, 2, 3, true);
+        expect(() => {
+            gameBoard.placeShip('test2', 2, 2, 3, true);
+        }).toThrow('Overlapping ships error');
+    });
+});
+
+// gameBoard receiveAttack() testing
+test('GameBoard receiveAttack() works when ship is attacked', () => {
+    let gameBoard = new GameBoard();
+    gameBoard.placeShip('test', 2, 2, 3, true);
+    gameBoard.receiveAttack(2, 2);
+    gameBoard.receiveAttack(3, 2);
+    let attackedShip = gameBoard.shipsMap.get('test');
+    expect(attackedShip.timesHit).toEqual(2);
+});
+
+// testing errors for receiveAttack()
+
+describe('GameBoard receiveAttack() error handling', () => {
+    let gameBoard;
+
+    //creates a new independent gameboard before each test
+    beforeEach(() => {
+        gameBoard = new GameBoard();
+    });
+
+    test('GameBoard throws error if you attack a space where you have already destroyed a ship', () => {
+        gameBoard.placeShip('test', 2, 2, 3, true);
+        gameBoard.receiveAttack(2, 2);
+        expect(() => {
+            gameBoard.receiveAttack(2, 2);
+        }).toThrow('ship has already been hit here');
+    });
+
+    test('GameBoard throws error if you attack a space where you have already missed', () => {
+        gameBoard.placeShip('test', 2, 2, 3, true);
+        gameBoard.receiveAttack(4, 4);
+        expect(() => {
+            gameBoard.receiveAttack(4, 4);
+        }).toThrow('target has already been missed here');
+    });
+});
+
+describe('GameBoard isSunk testing', () => {
+    beforeEach(() => {
+        gameBoard = new GameBoard();
+    });
+    test('GameBoard isSunk works when ship is sunk', () => {
+        // let gameBoard = new GameBoard();
+        gameBoard.placeShip('test', 2, 2, 3, true);
+        gameBoard.receiveAttack(2, 2);
+        gameBoard.receiveAttack(3, 2);
+        gameBoard.receiveAttack(4, 2);
+        // let attackedShip = gameBoard.shipsMap.get('test');
+        expect(gameBoard.shipsSunk()).toBe(true);
+    });
+    test('GameBoard isSunk works when ship is not sunk', () => {
+        // let gameBoard = new GameBoard();
+        gameBoard.placeShip('test', 2, 2, 3, true);
+        gameBoard.receiveAttack(2, 2);
+        gameBoard.receiveAttack(3, 2);
+        // let attackedShip = gameBoard.shipsMap.get('test');
+        expect(gameBoard.shipsSunk()).toBe(false);
     });
 });
